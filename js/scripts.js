738cd005dc5bcb4cf63c659f916ddfa8c70dc731
Jarret Jeter <jarretjeter@yahoo.com>
2022-04-01 09:54:41 -0700

    add output text for when button is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-  
+  $("form#form").submit(function(){
+    event.preventDefault();
+
+    console.log("hi");
+    let output = document.getElementById("output");
+    let response = "hello";
+    output.insertAdjacentText('beforeend', response);
+    
+  });
+
 });
 
 

a6e8ff9b0f04820624027adb57581a485c78a934
Jarret Jeter <jarretjeter@yahoo.com>
2022-04-01 10:56:10 -0700

    output text fix

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,19 @@ $(document).ready(function() {
   $("form#form").submit(function(){
     event.preventDefault();
 
-    console.log("hi");
+    let userInput = $("input#user-input").val();
+    let text = document.createElement("p");
+    let response;
+    text.append(userInput);
     let output = document.getElementById("output");
-    let response = "hello";
-    output.insertAdjacentText('beforeend', response);
-    
+    output.appendChild(text);
+
+
+    // let output = document.getElementById("output");
+    // let response = <p>Hello</p>
+    // // output.removeAttribute("id");
+    // output.insertAdjacentText('beforeend', response);
+    // // $().remove();
   });
 
 });
